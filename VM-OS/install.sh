d15fccedf66a4e3be65a0ff118b5aa874bc2f379
Michael <FakieNZ@users.noreply.github.com>
2021-08-02 22:52:53 +1200

    install.sh updated

--- VM-OS/install.sh
+++ VM-OS/install.sh
@@ -1,4 +1,4 @@
 rm packagelist.txt
-wget https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/packagelist.txt
+wget -q https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/packagelist.txt
 sed 's/#.*//' packagelist.txt | xargs sudo apt install -y
 rm packagelist.txt
