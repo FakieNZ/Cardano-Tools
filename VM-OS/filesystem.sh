5829f798fbc6771b5f481265d215e11b3d5477c5
Michael <FakieNZ@users.noreply.github.com>
2021-08-03 23:01:23 +1200

    filesystem.sh updated

--- VM-OS/filesystem.sh
+++ VM-OS/filesystem.sh
@@ -1,3 +1,2 @@
-mkdir -p /home/dostoevsky/.local/bin/backup
-mkdir -p /home/dostoevsky/.local/bin/retired
-mkdir -p /usr/local/lib/pkgconfig
+sudo mkdir -p /home/dostoevsky/.local/bin/{backup,retired}
+sudo mkdir -p /usr/local/lib/pkgconfig
