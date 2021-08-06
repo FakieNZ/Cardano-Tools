f8d52e800107d2e2f47d95e7e2760b458e3e9a10
Michael <FakieNZ@users.noreply.github.com>
2021-08-06 21:36:56 +1200

    install.sh updated to installDependencies.sh

--- VM-OS/installDependencies.sh
+++ VM-OS/installDependencies.sh
@@ -1,4 +1,4 @@
-rm packagelist.txt
+rm -f packagelist.txt
 wget -q https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/packagelist.txt
 sed 's/#.*//' packagelist.txt | xargs sudo apt install -y
-rm packagelist.txt
+rm -f packagelist.txt
