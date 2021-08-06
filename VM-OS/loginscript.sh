47111d6d9e198ed8ff15945220f4808992eb5971
Michael <FakieNZ@users.noreply.github.com>
2021-08-06 22:30:13 +1200

    loginscript.sh updated

--- VM-OS/loginscript.sh
+++ VM-OS/loginscript.sh
@@ -1,4 +1,1 @@
-export PATH="~/.local/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 wget -q -N https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/.bashrc
