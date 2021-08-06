f89ac72fb2dd9382ccca592fe0bf0ebf6beaa4cf
Michael <FakieNZ@users.noreply.github.com>
2021-08-06 22:32:28 +1200

    installCabal.sh updated

--- VM-OS/installCabal.sh
+++ VM-OS/installCabal.sh
@@ -2,5 +2,7 @@ wget https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.
 tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
 rm cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz cabal.sig
 sudo mv cabal ~/.local/bin/
-cabal update
-cabal --version
+cd ~/.local/bin/
+./cabal update
+./cabal --version
+cd ~
