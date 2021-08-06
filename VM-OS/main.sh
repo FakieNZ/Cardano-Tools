32aa20f316e153cf15bdad32f4a50a5ee0884b78
Michael <FakieNZ@users.noreply.github.com>
2021-08-06 21:55:47 +1200

    main.sh added

---
+++ VM-OS/main.sh
@@ -0,0 +1,20 @@
+tput setaf 2; echo PATCHING UBUNTU ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/patch.sh)
+
+tput setaf 2; echo INSTALLING DEPENDENCIES ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installDependencies.sh)
+
+tput setaf 2; echo CREATE FILE STRUCTURE ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/filesystem.sh)
+
+tput setaf 2; echo UPDATE BASH PROFILE ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/loginscript.sh)
+
+tput setaf 2; echo INSTALLING CABAL ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installCabal.sh)
+
+tput setaf 2; echo INSTALLING GHC ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installGHC.sh)
+
+tput setaf 2; echo INSTALLING LIBSODIUM ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installLibsodium.sh)
