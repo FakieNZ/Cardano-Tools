5191c878912f67747079be85b40e6cbf2dd8744c
Michael <FakieNZ@users.noreply.github.com>
2021-08-21 15:07:48 +1200

    filesystem.sh Updated

--- VM-OS/filesystem.sh
+++ VM-OS/filesystem.sh
@@ -1,7 +1,7 @@
-sudo mkdir -p /home/dostoevsky/.local/bin/{backup,retired}
-sudo mkdir -p /usr/local/lib/pkgconfig
-sudo mkdir -p /home/dostoevsky/relay/db
+mkdir -p /home/dostoevsky/.local/bin/{backup,retired}
+mkdir -p /usr/local/lib/pkgconfig
+mkdir -p /home/dostoevsky/relay/db
 
-sudo chown -R dostoevsky /home/dostoevsky/relay*
-sudo chmod 755 /home/dostoevsky/relay
-sudo chmod 755 /home/dostoevsky/relay/db
+#sudo chown -R dostoevsky /home/dostoevsky/relay*
+#sudo chmod 755 /home/dostoevsky/relay
+#sudo chmod 755 /home/dostoevsky/relay/db
