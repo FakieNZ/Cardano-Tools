81ea5f4f058a521769419c3435de9499e50fc689
Michael <FakieNZ@users.noreply.github.com>
2021-08-21 18:24:59 +1200

    VM-APP-1.28.0\configure-testnet.sh Updated

--- VM-APP-1.28.0/configure-testnet.sh
+++ VM-APP-1.28.0/configure-testnet.sh
@@ -2,3 +2,4 @@ wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-fi
 wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-shelley-genesis.json -P ~/relay
 wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-byron-genesis.json -P ~/relay
 wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-topology.json -P ~/relay
+wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-alonzo-genesis.json -P ~/relay
