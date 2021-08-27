c6dc9ca67683367ae3aebf34bb2f22a76edcfb45
Michael <FakieNZ@users.noreply.github.com>
2021-08-28 10:07:45 +1200

    installGHC8.10.7.sh Updated

--- VM-OS/installGHC8.10.7.sh
+++ VM-OS/installGHC8.10.7.sh
@@ -2,7 +2,7 @@ cd ~
 wget https://downloads.haskell.org/~ghc/8.10.7/ghc-8.10.7-x86_64-deb9-linux.tar.xz
 tar -xf ghc-8.10.7-x86_64-deb9-linux.tar.xz
 rm ghc-8.10.7-x86_64-deb9-linux.tar.xz
-cd ghc-8.10.2
+cd ghc-8.10.7
 ./configure
 sudo make install
 cd ..
