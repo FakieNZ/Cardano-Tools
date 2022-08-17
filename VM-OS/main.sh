2ba9fa5a20781eff8254f3f86688d17a0155fa52
Michael <FakieNZ@users.noreply.github.com>
2022-08-17 21:34:33 +1200

    VM-APP-1.35.3 Added

--- VM-OS/main.sh
+++ VM-OS/main.sh
@@ -19,5 +19,8 @@ bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master
 tput setaf 2; echo INSTALLING LIBSODIUM ; tput sgr0
 bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installLibsodium.sh)
 
+tput setaf 2; echo INSTALLING SECP256K1 ; tput sgr0
+bash <(wget -qO - https://raw.githubusercontent.com/FakieNZ/Cardano-Tools/master/VM-OS/installsecp256k1.sh)
+
 tput setaf 2; echo REBOOTING SERVER; tput sgr0
 sudo shutdown -r 1 
